b4198a1b54b36f790438793175d19755deb181b4
Jesse Dailey <jesse.dailey@gmail.com>
2014-04-14 10:40:57 -0400

    add some shows, filter out 720p versions, add auto-launching (uTorrent by default)

--- get-filtered.sh
+++ get-filtered.sh
@@ -4,11 +4,14 @@ SHOWS=`cat my-shows`
 
 RECENT=`./get-recent.sh`
 
+LAUNCH='/c/Program Files (x86)/uTorrent/uTorrent.exe'
+
 for show in $SHOWS; do
 	echo --- $show ---
 	for recent in $RECENT; do
 		if echo $recent | grep $show; then
 			echo $recent
+			"${LAUNCH}" $recent
 		fi
 	done
 done | grep -v 720p | uniq
--- get-recent.sh
+++ get-recent.sh
@@ -3,4 +3,5 @@
 wget -q http://eztv.it/ -O - \
 	| grep dn= \
 	| sed -E 's@.* href="(magnet:[^"]+)" class="magnet" title="Magnet Link"></a>.*@\1@'
+	| grep -v 720p
 	# | sed -E 's@.*dn=([^&]+)&.*@\1@'
